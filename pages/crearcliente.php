0efe413d2ce1cad47513dedcd55c0e2bba633b63
juancruzdauberte <juandauberte@gmail.com>
2024-11-24 21:18:40 -0300

    add validacion cuil-cuit

--- pages/crearcliente.php
+++ pages/crearcliente.php
@@ -28,11 +28,11 @@ include '..//querys/clientes/createCliente.php';
                 <div class="row mb-3">
                     <div class="col">
                         <label for="nombre">Nombre:</label>
-                        <input type="text" class="form-control" name="nombre" id="nombre" required maxlength="50"  pattern="[A-Za-z\s]+"  title="Debe ingresar carácteres.">
+                        <input type="text" class="form-control" name="nombre" id="nombre" required maxlength="50"  pattern="[A-Za-z\s]+"  title="Debe ingresar carácteres." value="<?php echo isset($nombre) ? htmlspecialchars($nombre) : ''; ?>">
                     </div>
                     <div class="col">
                         <label for="apellido">Apellido:</label>
-                        <input type="text" class="form-control" name="apellido" id="apellido" required maxlength="50"  pattern="[A-Za-z\s]+" title="Debe ingresar carácteres.">
+                        <input type="text" class="form-control" name="apellido" id="apellido" required maxlength="50"  pattern="[A-Za-z\s]+" title="Debe ingresar carácteres." value="<?php echo isset($apellido) ? htmlspecialchars($apellido) : ''; ?>">
                     </div>
                 </div>
 
@@ -40,11 +40,14 @@ include '..//querys/clientes/createCliente.php';
                 <div class="row mb-3">
                     <div class="col">
                         <label for="cuil_cuit">CUIL / CUIT:</label>
-                        <input type="text" class="form-control" name="cuil_cuit" id="cuil_cuit" required minlength="10" maxlength="10" pattern="\d+" title="Debe ingresar números.">
+                        <input type="text" class="form-control" name="cuil_cuit" id="cuil_cuit" required minlength="10" maxlength="10" pattern="\d+" title="Debe ingresar números." value="<?php echo isset($cuil_cuit) ? htmlspecialchars($cuil_cuit) : ''; ?>">
+                        <?php if (isset($error_message)) { ?>
+                            <span id="cuilError" style="color: red;"><?php echo $error_message; ?></span>
+                        <?php } ?>
                     </div>
                     <div class="col">
                         <label for="razon_social">Razón Social:</label>
-                        <input type="text" class="form-control" name="razon_social" id="razon_social" required maxlength="100"  pattern="[A-Za-z\s]+"  title="Debe ingresar carácteres.">
+                        <input type="text" class="form-control" name="razon_social" id="razon_social" required maxlength="100"  pattern="[A-Za-z\s]+"  title="Debe ingresar carácteres." value="<?php echo isset($razon_social) ? htmlspecialchars($razon_social) : ''; ?>">
 
                     </div>
                 </div>
@@ -53,11 +56,11 @@ include '..//querys/clientes/createCliente.php';
                 <div class="row mb-3">
                     <div class="col">
                         <label for="telefono">Teléfono:</label>
-                        <input type="text" class="form-control" name="telefono" id="telefono" required maxlength="20" pattern="\d+" title="Debe ingresar números.">
+                        <input type="text" class="form-control" name="telefono" id="telefono" required maxlength="20" pattern="\d+" title="Debe ingresar números." value="<?php echo isset($telefono) ? htmlspecialchars($telefono) : ''; ?>">
                     </div>
                     <div class="col">
                         <label for="email">Email:</label>
-                        <input type="email" class="form-control" name="email" id="email" required maxlength="100">
+                        <input type="email" class="form-control" name="email" id="email" required maxlength="100" value="<?php echo isset($email) ? htmlspecialchars($email) : ''; ?>">
                         <span id="error" style="color: red; display: none;">Correo inválido</span>
                     </div>
                 </div>
