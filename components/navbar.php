741989749e3bb2fa993d3d82a83e9d3d799605ef
Nahuel_Dumbsky <dumbskynahuel@gmail.com>
2024-11-27 11:07:48 -0300

    remocion de estilos inline en navbar.php y ajuste de stylesheet.php

--- components/navbar.php
+++ components/navbar.php
@@ -1,57 +1,29 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DannaFox</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../styles/stylesheet.css?v=1.0">
-    <style>
-        .btn-steel-blue {
-            background-color: #4682B4; /* Color steel blue */
-            border: none;
-            color: #4682b4;
-        }
-
-        .btn-steel-blue:hover {
-            background-color: #5a9bd3; /* Color más claro al hacer hover */
-        }
-
-        /* Estilo del menú desplegable */
-        .dropdown-menu {
-            background-color: #ffffff; /* Fondo blanco */
-            border: 1px solid #ddd;   /* Borde tenue */
-        }
-
-        /* Botones dentro del menú (Clientes, Campañas) */
-        .dropdown-item {
-            color: #000; /* Texto negro */
-            
-        }
-
-
-        /* Estilo del botón "Cerrar Sesión" */
-        .dropdown-item.text-danger {
-            color: #d9534f; /* Texto rojo por defecto */
-        }
-
-
-    </style>
 </head>
+
 <body>
     <?php
-    function mostrarBotonVolver() {
+    function mostrarBotonVolver()
+    {
         $exclusiones = ['/', '/DannaFox/index.php', '/DannaFox/login.php'];
         return !in_array($_SERVER['REQUEST_URI'], $exclusiones);
     }
     ?>
     <header class="header">
         <?php if (mostrarBotonVolver()): ?>
-            <button onclick="history.back()" class="btn btn-yellow  btn-sm" style="margin-left: 20px;">Volver atrás</button>
+            <button onclick="history.back()" class="btn btn-yellow btn-sm" style="margin-left: 20px;">Volver atrás</button>
         <?php endif; ?>
 
         <div class="logo">
-        <a class="navbar-brand" href="/DannaFox/index.php"><span class="logo-text"><span class="yellow-text">D</span>anna<span class="yellow-text">F</span>ox</span></a>
+            <a class="navbar-brand" href="/DannaFox/index.php"><span class="logo-text"><span class="yellow-text">D</span>anna<span class="yellow-text">F</span>ox</span></a>
         </div>
 
         <nav class="navbar navbar-dark">
@@ -61,13 +33,13 @@
                     <button class="btn btn-steel-blue navbar-toggler" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    
+
                     <!-- Menú desplegable -->
                     <ul class="dropdown-menu dropdown-menu-end">
                         <li>
                             <a class="dropdown-item text-black" href="/DannaFox/pages/clientes.php">Clientes</a>
                         </li>
-                        <li >
+                        <li>
                             <a class="dropdown-item text-black" href="/DannaFox/pages/campanias.php">Campañas</a>
                         </li>
                         <li>
@@ -84,4 +56,5 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
